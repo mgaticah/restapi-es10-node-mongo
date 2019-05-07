0003771f1cc29ec8321d3f386d71a916846b7192
mgaticah <manuel.gatica@datco.cl>
2019-05-07 12:01:30 -0400

    uso de express para inicar un servidor escuchando en el puerto 3000

--- node-restapi-es10/src/index.js
+++ node-restapi-es10/src/index.js
@@ -1,2 +1,10 @@
-const test='test'
-console.log(test);
+import '@babel/polyfill';
+import app from './server';
+
+
+async function main() {
+    await app.listen(3000);
+    console.log('server on port:'+3000);
+}
+
+main();
