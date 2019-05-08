54e11948c9d5fcbb5c247ac3d5d8af9a718a5072
mgaticah <manuel.gatica@datco.cl>
2019-05-07 20:07:25 -0400

    implementando rutas en index.route

--- node-restapi-es10/src/server.js
+++ node-restapi-es10/src/server.js
@@ -4,6 +4,6 @@ const app=express();
 //Settings
 app.set('port', process.env.PORT||3000);
 //Routes
-app.get('/',(req,res)=>{ res.send('running')});
-
+import IndexRoutes from './Routes/index.routes';
+app.use(IndexRoutes);
 export default app;
